f71f88d2745d6c19d5c10be98b038ada239113c5
Fan Pu <zengfanpu@gmail.com>
2023-04-18 22:55:42 -0400

    add benchmark for batch size 50

--- dynpartition/partitioner/rl_partitioner.py
+++ dynpartition/partitioner/rl_partitioner.py
@@ -1,1 +1,109 @@
 # TODO: Fanpu
+import torchvision.models as models
+import timeit
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision.models.resnet import ResNet, Bottleneck
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torchvision.models.resnet import ResNet, Bottleneck
+
+num_classes = 1000
+
+
+class ModelParallelResNet50(ResNet):
+    def __init__(self, *args, **kwargs):
+        super(ModelParallelResNet50, self).__init__(
+            Bottleneck, [3, 4, 6, 3], num_classes=num_classes, *args, **kwargs)
+
+        self.seq1 = nn.Sequential(
+            self.conv1,
+            self.bn1,
+            self.relu,
+            self.maxpool,
+
+            self.layer1,
+            self.layer2,
+            self.layer3,
+            self.layer4,
+        ).to('cuda:0')
+
+        self.seq2 = nn.Sequential(
+            self.avgpool,
+        ).to('cpu')
+
+        self.fc.to('cpu')
+
+    def forward(self, x):
+        x = self.seq2(self.seq1(x).to('cpu'))
+        return self.fc(x.view(x.size(0), -1))
+
+
+num_batches = 3
+batch_size = 50  # 120
+image_w = 128
+image_h = 128
+
+
+def train(model):
+    model.train(True)
+    loss_fn = nn.MSELoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+    one_hot_indices = torch.LongTensor(batch_size) \
+                           .random_(0, num_classes) \
+                           .view(batch_size, 1)
+
+    for _ in range(num_batches):
+        # generate random inputs and labels
+        inputs = torch.randn(batch_size, 3, image_w, image_h)
+        labels = torch.zeros(batch_size, num_classes) \
+                      .scatter_(1, one_hot_indices, 1)
+
+        # run forward pass
+        optimizer.zero_grad()
+        outputs = model(inputs.to('cuda:0'))
+
+        # run backward pass
+        labels = labels.to(outputs.device)
+        loss_fn(outputs, labels).backward()
+        optimizer.step()
+
+
+plt.switch_backend('Agg')
+
+num_repeat = 10
+
+stmt = "train(model)"
+
+setup = "model = ModelParallelResNet50()"
+mp_run_times = timeit.repeat(
+    stmt, setup, number=1, repeat=num_repeat, globals=globals())
+mp_mean, mp_std = np.mean(mp_run_times), np.std(mp_run_times)
+
+setup = "import torchvision.models as models;" + \
+        "model = models.resnet50(num_classes=num_classes).to('cuda:0')"
+rn_run_times = timeit.repeat(
+    stmt, setup, number=1, repeat=num_repeat, globals=globals())
+rn_mean, rn_std = np.mean(rn_run_times), np.std(rn_run_times)
+
+
+def plot(means, stds, labels, fig_name):
+    fig, ax = plt.subplots()
+    ax.bar(np.arange(len(means)), means, yerr=stds,
+           align='center', alpha=0.5, ecolor='red', capsize=10, width=0.6)
+    ax.set_ylabel('ResNet50 Execution Time (Second)')
+    ax.set_xticks(np.arange(len(means)))
+    ax.set_xticklabels(labels)
+    ax.yaxis.grid(True)
+    plt.tight_layout()
+    plt.savefig(fig_name)
+    plt.close(fig)
+
+
+plot([mp_mean, rn_mean],
+     [mp_std, rn_std],
+     ['Model Parallel', 'Single GPU'],
+     'mp_vs_rn.png')
