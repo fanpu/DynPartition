93faac520b9d3938b79c1503947098f859713fcb
Vivswan Shah <58091053+Vivswan@users.noreply.github.com>
2023-04-24 09:30:54 -0400

    add TreeLSTM

--- dynpartition/models/TreeLSTM.py
+++ dynpartition/models/TreeLSTM.py
@@ -1,2 +1,4 @@
 # TODO: Vivswan
-
+# https://github.com/Vivswan/TreeLSTMSentiment
+# Branch: updates_for_python3_and_pytorch_2.0.0
+# Data: https://drive.google.com/file/d/1F7Xeb4sBMJ3nsZ5Dj1LJh-XySaD9uMju/view?usp=share_link
