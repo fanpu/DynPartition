64797b586bab869328925d445ac341002473da2d
Vivswan Shah <58091053+Vivswan@users.noreply.github.com>
2023-04-24 22:14:15 -0400

    added links for TreeLSTM

--- dynpartition/models/TreeLSTM.py
+++ dynpartition/models/TreeLSTM.py
@@ -2,3 +2,6 @@
 # https://github.com/Vivswan/TreeLSTMSentiment
 # Branch: updates_for_python3_and_pytorch_2.0.0
 # Data: https://drive.google.com/file/d/1F7Xeb4sBMJ3nsZ5Dj1LJh-XySaD9uMju/view?usp=share_link
+# Paper: https://arxiv.org/abs/1503.00075
+# Survey: https://arxiv.org/abs/2102.04906
+# Constituency Parsing vs Dependency Parsing: https://www.baeldung.com/cs/constituency-vs-dependency-parsing
