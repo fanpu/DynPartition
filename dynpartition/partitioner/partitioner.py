d5e8ecfc2c8addfa408bdef26171d731630a8b59
Vivswan Shah <58091053+Vivswan@users.noreply.github.com>
2023-04-28 05:01:30 -0400

    added random partitioner

--- dynpartition/partitioner/partitioner.py
+++ dynpartition/partitioner/partitioner.py
@@ -1,4 +1,16 @@
-# TODO: Yudong
+import math
 
-def main():
-    pass
+import numpy as np
+import torch
+
+
+def random_distribution(dataset, devices):
+    for i in range(len(devices)):
+        devices[i] = torch.device(devices[i])
+
+    for tree in dataset:
+        for i in tree.get_all_nodes():
+            rand1 = math.floor(np.random.rand() * len(devices))
+            rand2 = math.floor(np.random.rand() * len(devices))
+            i.device_for_state = devices[rand1]
+            i.device_for_output = devices[rand2]
