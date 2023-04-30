bdd55e0d5e212e38a392a70d9e4321cef2a9b39d
Vivswan Shah <58091053+Vivswan@users.noreply.github.com>
2023-04-30 02:38:55 -0400

    true random

--- dynpartition/partitioner/partitioner.py
+++ dynpartition/partitioner/partitioner.py
@@ -25,16 +25,74 @@ def single_device_run(
 def random_distribution(
         trees: List[Tree],
         devices: List[Union[str, torch.device]],
-) -> List[Tree]:
+        max_layer_per_device: Optional[List[int]] = None
+):
+    modules = {"leaf_module", "composer", "output_module"}
+    max_layer_per_device = copy.deepcopy(max_layer_per_device)
+    if max_layer_per_device is None:
+        max_layer_per_device = [9999] * len(devices)
+
+    if len(devices) != len(max_layer_per_device):
+        raise ValueError("The length of devices and max_layer_per_device "
+                         "must be the same")
+
     for i in range(len(devices)):
         devices[i] = torch.device(devices[i])
+        max_layer_per_device[i] = int(max(max_layer_per_device[i], 0))
+        max_layer_per_device[i] = int(min(max_layer_per_device[i], 3))
+
+    if sum(max_layer_per_device) < 3:
+        raise ValueError("The sum of max_layer_per_device must be at least 3")
+
+    device_availability = {}
+    allocated_module_list = {}
+    for i in range(len(devices)):
+        if max_layer_per_device[i] <= 0:
+            continue
+
+        device_availability[devices[i]] = max_layer_per_device[i]
+        allocated_module_list[devices[i]] = set()
+
+    for i in modules:
+        choice = np.random.choice(list(device_availability.keys()))
+        allocated_module_list[choice].add(i)
+        device_availability[choice] -= 1
+
+        if device_availability[choice] <= 0:
+            device_availability.pop(choice)
+
+    while len(device_availability) > 0:
+        choice = np.random.choice(list(device_availability.keys()))
+        module = np.random.choice(list(modules - allocated_module_list[choice]))
+        allocated_module_list[choice].add(module)
+        device_availability[choice] -= 1
+
+        if device_availability[choice] <= 0:
+            device_availability.pop(choice)
+
+    module_to_device = {}
+    for device, module_list in allocated_module_list.items():
+        for module in module_list:
+            if module not in module_to_device:
+                module_to_device[module] = []
+
+            module_to_device[module].append(device)
 
     trees = copy.deepcopy(trees)
     for tree in trees:
         for i in tree.get_all_nodes():
-            rand1 = math.floor(np.random.rand() * len(devices))
-            rand2 = math.floor(np.random.rand() * len(devices))
-            i.device_for_state = devices[rand1]
-            i.device_for_output = devices[rand2]
+            if i.is_leaf():
+                i.device_for_state = np.random.choice(
+                    module_to_device["leaf_module"]
+                )
+            else:
+                i.device_for_state = np.random.choice(
+                    module_to_device["composer"]
+                )
+
+            i.device_for_output = np.random.choice(
+                module_to_device["output_module"]
+            )
 
     return trees
+
