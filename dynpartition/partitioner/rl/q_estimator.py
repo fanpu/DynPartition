e233a0bdb56b037a0261cf9d0652b8be8cbf3650
Vivswan Shah <58091053+Vivswan@users.noreply.github.com>
2023-05-01 05:33:50 -0400

    fix

--- dynpartition/partitioner/rl/q_estimator.py
+++ dynpartition/partitioner/rl/q_estimator.py
@@ -4,7 +4,7 @@ import torch
 
 
 class FullyConnectedModel(torch.nn.Module):
-    def __init__(self, input_size, output_shape, num_layers=2):
+    def __init__(self, input_size, output_shape, num_layers=4):
         super().__init__()
         if isinstance(input_size, tuple):
             input_size = math.prod(input_size)
@@ -16,7 +16,7 @@ class FullyConnectedModel(torch.nn.Module):
         self.layers.append(torch.nn.Flatten(start_dim=0))
         self.layers.append(torch.nn.Linear(input_size, 16))
         self.layers.append(torch.nn.ReLU())
-        for _ in range(num_layers - 1):
+        for _ in range(num_layers - 2):
             self.layers.append(torch.nn.Linear(16, 16))
             self.layers.append(torch.nn.ReLU())
         self.layers.append(torch.nn.Linear(16, math.prod(output_shape)))
