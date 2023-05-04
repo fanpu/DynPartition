8644d114b459945838bbbf6f22355e49b64c5054
Vivswan Shah <58091053+Vivswan@users.noreply.github.com>
2023-05-04 00:58:15 -0400

    added 10000 rl data

--- dynpartition/run.sh
+++ dynpartition/run.sh
@@ -1,7 +1,7 @@
 module load cuda
 conda activate ~/.conda_envs/dynpartition
-
 export PYTHONPATH="${HOME}/DynPartition"
+
 python3 "${HOME}/DynPartition/dynpartition/partitioner/run_single_device.py"
 python3 "${HOME}/DynPartition/dynpartition/partitioner/run_single_device.py" --with-cpu
 python3 "${HOME}/DynPartition/dynpartition/x/test_data_trasfer_speeds.py" --with-cpu
